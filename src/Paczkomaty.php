c9405cfcf55533dda8e3dda5a400ab090b65b0b3
seszcze <sebastian@ably.pl>
2018-01-10 23:13:41 +0100

    added new methods and phpdoc

--- src/Paczkomaty.php
+++ src/Paczkomaty.php
@@ -3,12 +3,20 @@
 
 namespace Lari\Furgonetka;
 
-use App\User;
-
+/**
+ * @author    Sebastian Szczepański
+ * @copyright ably
+ */
 class Paczkomaty
 {
+    /**
+     * @var
+     */
     private $client;
 
+    /**
+     * @var array
+     */
     protected $query = [];
 
     /**
@@ -22,19 +30,70 @@ class Paczkomaty
     }
 
     /**
-     * @param \App\User $user
-     * @return $this
+     * @param string $postcode
+     * @return Paczkomaty $this
      */
-    public function extract(User $user)
+    public function setPostcode($postcode)
     {
-        $this->query = [
-            'postcode' => $user->postal,
-            'city' => $user->city,
-            'street' => $user->street
-        ];
+        $this->setQueryKey('postcode', $postcode);
+
         return $this;
     }
 
+    /**
+     * @param string $city
+     * @return Paczkomaty $this
+     */
+    public function setCity(string $city)
+    {
+        $this->setQueryKey('city', $city);
+
+        return $this;
+    }
+
+    /**
+     * @param string $street
+     * @return Paczkomaty $this
+     */
+    public function setStreet($street)
+    {
+        $this->setQueryKey('street', $street);
+
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return Paczkomaty $this
+     */
+    private function setQueryKey($key, $value): Paczkomaty
+    {
+        $this->query = [
+            $key => $value,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * @param User
+     * @return $this
+     */
+    public function setUser($user)
+    {
+        $this->query = [
+            'postcode' => $user->postal,
+            'city'     => $user->city,
+            'street'   => $user->street,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
     public function get()
     {
         return $this->client->get('getPaczkomaty', $this->query);
